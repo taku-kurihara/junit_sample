9e1e73b60940d3e5e991422656a6279ecb1591fa
t-kurihara <takuya.kurihara00@gmail.com>
2018-09-28 17:16:27 +0900

    [change] JavaDoc追記.

--- src/main/java/controller/ModelController.java
+++ src/main/java/controller/ModelController.java
@@ -13,12 +13,21 @@ public class ModelController {
         this.modelService = modelService;
     }
 
+    /**
+     * IDの{@link model.Model}を取得する.
+     * @param id {@link model.Model}のID.
+     * @return {@link model.Model}の文字列.
+     */
     public String findModel(final int id) {
         return modelService
                 .getModelById(id)
                 .toString();
     }
 
+    /**
+     * {@link model.Model}の一覧を取得.
+     * @return {@link java.util.List<model.Model>}の文字列.
+     */
     public String findAll() {
 
         return modelService
